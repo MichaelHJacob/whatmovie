3e89a7e945d076873dcc6429de6ebb4ad01c6bed
MichaelHJacob <michael_h.jacob@hotmail.com>
2023-10-13 01:10:57 -0300

    update chave

--- src/components/chave.tsx
+++ src/components/chave.tsx
@@ -4,6 +4,7 @@ export default function Chave(){
 
     return(<>
         <h1>componente cliente :</h1>
+        <p>testadno visibilidade</p>
         <h1> {process.env.DB_API_URL}</h1>
         </>
     )
