6793ab975d33a56f552a93effb273efb52db1de8
MichaelHJacob <michael_h.jacob@hotmail.com>
2023-10-13 01:08:53 -0300

    chave

--- src/components/chave.tsx
+++ src/components/chave.tsx
@@ -4,7 +4,7 @@ export default function Chave(){
 
     return(<>
         <h1>componente cliente :</h1>
-        {/* <h1> {process.env.DB_API_URL}</h1> */}
+        <h1> {process.env.DB_API_URL}</h1>
         </>
     )
 
