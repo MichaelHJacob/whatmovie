1a626c0cabe225814871ee9a52874598e50ad5e1
MichaelHJacob <michael_h.jacob@hotmail.com>
2024-01-19 21:30:47 -0300

    correção

--- src/app/api/genres/route.tsx
+++ src/app/api/genres/route.tsx
@@ -14,22 +14,7 @@ export async function GET() {
   }
 
   const data = await res.json();
-  // console.log(data)
+
   return Response.json( data );
 }
 
-// export async function GetMensagem() {
-//   return new Response ('Hello, Next.js')
-// }
-
-// export async function GET() {
-//   const res = await fetch('https://data.mongodb-api.com/...', {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'API-Key': process.env.DATA_API_KEY,
-//     },
-//   })
-//   const data = await res.json()
- 
-//   return Response.json({ data })
-// }
