684b42da1fbfb7e44797d04405a9c508451573db
MichaelHJacob <michael_h.jacob@hotmail.com>
2024-07-11 20:57:50 -0300

    Update template.tsx

--- src/app/filter/template.tsx
+++ src/app/filter/template.tsx
@@ -4,27 +4,21 @@ import { LoadingCards } from "@/components/frame";
 import { Suspense } from "react";
 import { NavBar } from "@/components/comps";
 
-export default function Template({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
-    <NavBar fixed/>
-    <FilterSideMenu>
-    <Suspense
-      fallback={
-        <Container>
+      <NavBar fixed />
+      <Suspense
+        fallback={
+          <Container>
             <div className="w-full gridTemplateSpace itens-center blockContainer">
               <LoadingCards />
             </div>
-        </Container>
-      }
-    >
-      {children}
-    </Suspense>
-    </FilterSideMenu>
+          </Container>
+        }
+      >
+        <FilterSideMenu>{children}</FilterSideMenu>
+      </Suspense>
     </>
   );
 }
