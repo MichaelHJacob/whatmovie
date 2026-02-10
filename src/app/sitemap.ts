9fbfbaae711f01c85261f54d2e805c7c0e9aeaf3
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-02-10 18:46:50 -0300

    refactor(seo): simplify sitemap fields and fix invalid lastmodified
    - Remove deprecated 'priority' and 'changeFrequency' fields.
    - Remove 'lastModified' from movie pages.
    - Switch movie source from 'upcoming' to 'popular' for better relevance.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,30 +1,24 @@
 import { MetadataRoute } from "next";
 
-import { getNowPlaying } from "@/lib/api/tmdb/use-cases/getNowPlaying";
+import { getPopular } from "@/lib/api/tmdb/use-cases/getPopular";
+import getToday from "@/lib/utils/getToday";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const [data] = await getNowPlaying();
+  const [data] = await getPopular();
 
   const movies = data?.results?.map((movie) => movie.id) || [];
 
   const moviesUrls = movies.map((movie) => ({
     url: `https://whatmovie.com.br/movie/${movie}`,
-    lastModified: new Date(),
-    priority: 0.8,
   }));
 
   return [
     {
       url: "https://whatmovie.com.br",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
+      lastModified: getToday(),
     },
     {
       url: "https://whatmovie.com.br/filter",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.5,
     },
     ...moviesUrls,
   ];
