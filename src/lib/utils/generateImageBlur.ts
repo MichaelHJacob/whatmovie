ea4e3c60231f5e933fa94a3808a5776f02fbc748
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-02-12 19:03:17 -0300

    perf: update generateImageBlur to webp and 80 quality
    This reduces the average size of the embedded base64 image from 16kb to 3kb.

--- src/lib/utils/generateImageBlur.ts
+++ src/lib/utils/generateImageBlur.ts
@@ -8,20 +8,20 @@ export async function generateImageBlur(imageUrl: string): Promise<string> {
   const resizedBuffer = await sharp(buffer)
     .resize(300, 300, { fit: "cover", position: "center" })
     .extract({
-      left: 25,
+      left: 50,
       top: 50,
-      width: 250,
+      width: 200,
       height: 200,
     })
     .blur(50)
     .modulate({ brightness: 0.7, lightness: 30, saturation: 1.4 })
     .linear(0.6, -30)
-    .jpeg({ quality: 100 })
+    .webp({ quality: 80 })
     .toBuffer();
 
   if (!resizedBuffer) {
     throw new Error("Falha ao gerar imagem base64");
   }
 
-  return `data:image/jpeg;base64,${resizedBuffer.toString("base64")}`;
+  return `data:image/webp;base64,${resizedBuffer.toString("base64")}`;
 }
