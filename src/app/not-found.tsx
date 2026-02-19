e79a2d4a30ee3ee1ee2e69be1373904bc27daa75
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-02-18 22:10:42 -0300

    refactor(layout): move navigation bar from route layout to root layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,6 @@
 import ErrorPage from "@/components/error/ErrorPage";
 import Container from "@/components/layout/Container";
 import ListScrollController from "@/components/layout/ListScrollController";
-import NavBar from "@/components/layout/NavBar";
 import HTitle from "@/components/ui/HTitle";
 import ListMovie from "@/components/ui/ListMovie";
 import { getPopular } from "@/lib/api/tmdb/use-cases/getPopular";
@@ -9,32 +8,29 @@ import { getPopular } from "@/lib/api/tmdb/use-cases/getPopular";
 export default async function NotFound() {
   const [data] = await getPopular();
   return (
-    <>
-      <NavBar fixed />
-      <main>
-        <section>
-          <ErrorPage
-            as="div"
-            model="notFound"
-            textH1="Página não encontrada"
-            textCode="404"
-            textMsg1="Não conseguimos encontrar essa página"
-            textMsg2="mas talvez você encontre um filme interessante por aqui!"
-          />
-          {data?.results && (
-            <Container as="aside" className="relative">
-              <HTitle>Filmes mais acessados</HTitle>
-              <ListScrollController
-                id={"popular"}
-                length={data.results.length}
-                surface
-              >
-                <ListMovie data={data?.results} id={"popular"} />
-              </ListScrollController>
-            </Container>
-          )}
-        </section>
-      </main>
-    </>
+    <main>
+      <section>
+        <ErrorPage
+          as="div"
+          model="notFound"
+          textH1="Página não encontrada"
+          textCode="404"
+          textMsg1="Não conseguimos encontrar essa página"
+          textMsg2="mas talvez você encontre um filme interessante por aqui!"
+        />
+        {data?.results && (
+          <Container as="aside" className="relative">
+            <HTitle>Filmes mais acessados</HTitle>
+            <ListScrollController
+              id={"popular"}
+              length={data.results.length}
+              surface
+            >
+              <ListMovie data={data?.results} id={"popular"} />
+            </ListScrollController>
+          </Container>
+        )}
+      </section>
+    </main>
   );
 }
