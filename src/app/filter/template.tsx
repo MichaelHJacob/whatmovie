b29440a3f3bba934b29aa9d45e0b002b5b7b493a
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-02-18 22:10:42 -0300

    refactor(layout): move navigation bar from route layout to root layout

--- src/app/filter/template.tsx
+++ src/app/filter/template.tsx
@@ -2,20 +2,16 @@ import { Suspense } from "react";
 
 import FilterMenu from "@/app/filter/components/FilterMenu";
 import { QueryProvider } from "@/components/Providers";
-import NavBar from "@/components/layout/NavBar";
 
 type TemplateProps = { children: React.ReactNode };
 export const revalidate = 86400;
 
 export default function Template({ children }: Readonly<TemplateProps>) {
   return (
-    <>
-      <NavBar fixed />
-      <Suspense>
-        <FilterMenu>
-          <QueryProvider>{children}</QueryProvider>
-        </FilterMenu>
-      </Suspense>
-    </>
+    <Suspense>
+      <FilterMenu>
+        <QueryProvider>{children}</QueryProvider>
+      </FilterMenu>
+    </Suspense>
   );
 }
