0598786e09cac4454ef83f89ec979f20b55a2070
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-02-20 11:52:42 -0300

    refactor: remove support for switching styles via props

--- src/components/layout/NavBar/navbar.styles.ts
+++ src/components/layout/NavBar/navbar.styles.ts
@@ -5,22 +5,7 @@ export const navbarBase = tv({
     btnHeader:
       "flex min-h-8 appearance-none items-center gap-2 rounded-2xl px-2",
     btnText:
-      "textBtn text-base font-semibold leading-normal transition-colors duration-300",
-    icon: "box-content h-3 min-w-3 stroke-none p-[2px] transition-colors duration-300",
-  },
-  variants: {
-    dark: {
-      true: {
-        btnText: "text-nav-white hover:text-nav-white-hover",
-        icon: "fill-nav-white stroke-none hover:fill-nav-hover",
-      },
-      false: {
-        btnText: "text-nav hover:text-nav-hover",
-        icon: "fill-nav stroke-none hover:fill-nav-hover",
-      },
-    },
-  },
-  defaultVariants: {
-    dark: false,
+      "textBtn text-base font-semibold leading-normal text-nav transition-colors duration-300 hover:text-nav-hover",
+    icon: "box-content h-3 min-w-3 fill-nav stroke-none p-[2px] transition-colors duration-300 hover:fill-nav-hover",
   },
 });
