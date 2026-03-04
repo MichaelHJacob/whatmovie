dd3391509ac188339053f2a001d122ec7c6a5c2a
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-03-04 19:59:52 -0300

    refactor(movie): split page of the movie route into PageHero and Details components

--- src/app/movie/[movieId]/components/layout/Details/Translations.tsx
+++ src/app/movie/[movieId]/components/layout/Details/Translations.tsx
@@ -1,26 +1,34 @@
-import CardInformation from "@/app/movie/[movieId]/components/ui/CardInformation";
+import CardInformation from "@/app/movie/[movieId]/components/layout/Details/CardInformation";
 import { getMovieTranslations } from "@/lib/api/tmdb/use-cases/getMovieTranslations";
+import { tv } from "tailwind-variants";
 
 type TranslationsProps = {
   movieId: string;
 };
 
+const translationsStyles = tv({
+  slots: {
+    ul: "mb-2 grid w-full grid-cols-2 xs:grid-cols-3 sm:grid-cols-[repeat(auto-fit,_minmax(10rem,_1fr))]",
+    li: "data col-span-1 row-span-1",
+  },
+});
+
 export default async function Translations({
   movieId,
 }: Readonly<TranslationsProps>) {
   const [data] = await getMovieTranslations({ id: movieId });
-
   if (!data) return null;
+  const { ul, li } = translationsStyles();
 
   return (
     <div className="blockContainer-p">
       <CardInformation>
         <dt className="label mb-1">Traduções:</dt>
         <dd>
-          <ul className="mb-2 grid w-full grid-cols-2 xs:grid-cols-3 sm:grid-cols-[repeat(auto-fit,_minmax(10rem,_1fr))]">
+          <ul className={ul()}>
             {data.translations.map((value) => {
               return (
-                <li className="data col-span-1 row-span-1" key={value.name}>
+                <li className={li()} key={value.name}>
                   {value.name + "-" + value.iso_3166_1}
                 </li>
               );
