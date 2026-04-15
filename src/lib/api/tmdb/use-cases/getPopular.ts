3acfd5f0e324c2a47eae718767f36147ae74c3bc
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-04-14 23:41:56 -0300

    feat(api): implement custom discovery logic for popular movies

--- src/lib/api/tmdb/use-cases/getPopular.ts
+++ src/lib/api/tmdb/use-cases/getPopular.ts
@@ -2,7 +2,9 @@ import { API_ENDPOINTS } from "@/config/apiEndpoints";
 import { serverFetch } from "@/lib/api/tmdb/serverFetch";
 import { getLocalParams } from "@/lib/i18n/getLocaleParams";
 import { discoverSchema } from "@/lib/validation/discoverSchema";
+import { FilterSchemaType } from "@/lib/validation/filterSchema";
 import { GetUseCasesParams } from "@/types/globalTypes";
+import { flatten } from "flat";
 
 export async function getPopular({
   locale = "pt-BR",
@@ -10,9 +12,18 @@ export async function getPopular({
 }: GetUseCasesParams = {}) {
   const local = getLocalParams(locale);
 
-  const params = new URLSearchParams({ page: page.toString(), ...local });
+  const popularParams: FilterSchemaType = {
+    page: page.toString(),
+    ...local,
+    sort_by: "popularity.desc",
+    include_adult: "false",
+    include_video: "false",
+    certification: { lte: "16" },
+  };
 
-  const url = `${API_ENDPOINTS.moviesList.popular}?${params.toString()}`;
+  const params = new URLSearchParams(flatten(popularParams));
+
+  const url = `${API_ENDPOINTS.finding.filter}?${params.toString()}`;
 
   return serverFetch<typeof discoverSchema._output>({
     url,
