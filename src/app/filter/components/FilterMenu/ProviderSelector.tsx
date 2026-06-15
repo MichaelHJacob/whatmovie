241dd31c14451a78891bcc51f794262d51085fa6
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-06-15 18:56:10 -0300

    refactor: Implement shareable base style for menu fields

--- src/app/filter/components/FilterMenu/ProviderSelector.tsx
+++ src/app/filter/components/FilterMenu/ProviderSelector.tsx
@@ -1,6 +1,8 @@
+import { filterMenuBase } from "@/app/filter/components/FilterMenu/filterMenu.styles";
 import ClearSelected from "@/app/filter/components/ui/ClearSelected";
 import ProviderButton from "@/app/filter/components/ui/ProviderButton";
 import { TypeBtnProvider } from "@/types/globalTypes";
+import clsx from "clsx";
 
 type ProviderSelectorProps = {
   providers: TypeBtnProvider[];
@@ -15,22 +17,25 @@ export default function ProviderSelector({
   remove,
   clear,
 }: Readonly<ProviderSelectorProps>) {
+  const { field, title, topButton, innerField } = filterMenuBase();
   return (
-    <li>
-      <fieldset>
-        <legend className="blockContainer-px flex w-full items-center justify-between pb-[--gap] md:pb-[--gapMD]">
-          <span className="filter-label">Onde assistir:</span>
-          <ClearSelected onClear={clear} />
-        </legend>
-
-        <ul className="blockContainer-px grid h-auto w-full select-none grid-cols-[repeat(auto-fit,_2rem)] justify-between gap-2 max-sm:grid-cols-[repeat(auto-fit,_2.25rem)]">
-          {providers.map((value) => (
-            <li key={value.provider_id}>
-              <ProviderButton provider={value} add={add} remove={remove} />
-            </li>
-          ))}
-        </ul>
-      </fieldset>
-    </li>
+    <fieldset className={field()}>
+      <legend className={title()}>Onde assistir:</legend>
+      <div className={topButton()}>
+      <ClearSelected onClear={clear} />
+      </div>
+      <ul
+        className={clsx(
+          innerField(),
+          "grid grid-cols-[repeat(auto-fit,_2rem)] justify-between max-sm:grid-cols-[repeat(auto-fit,_2.25rem)]",
+        )}
+      >
+        {providers.map((value) => (
+          <li key={value.provider_id}>
+            <ProviderButton provider={value} add={add} remove={remove} />
+          </li>
+        ))}
+      </ul>
+    </fieldset>
   );
 }
