b5362127a632f5430a85e29bbce696bb0a4d7c95
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-06-30 15:52:25 -0300

    build(sst): initialize sst v4 platform and infrastructure config

---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "whatmovie",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    const web = new sst.aws.Nextjs("WhatMovieWeb");
+
+    return {
+      url: web.url,
+    };
+  },
+});
