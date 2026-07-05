23f523deb32c68660648a1b5b66a2813aa21dbcb
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-07-05 14:51:38 -0300

    build(sst): configure domain routing and DNS server

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -8,12 +7,26 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        cloudflare: { package: "@pulumi/cloudflare", version: "6.17.0" },
+        aws: { package: "@pulumi/aws", version: "7.36.0" },
+      },
     };
   },
   async run() {
     const tmdbApiToken = new sst.Secret("TmdbApiToken");
     const web = new sst.aws.Nextjs("WhatMovieWeb", {
       link: [tmdbApiToken],
+      path: "./",
+      domain:
+        $app.stage === "production"
+          ? {
+              name: "whatmovie.com.br",
+              dns: sst.cloudflare.dns({
+                proxy: true,
+              }),
+            }
+          : undefined,
     });
     return {
       url: web.url,
