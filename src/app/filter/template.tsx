2983fc7fa61ad0e9e4272fe82c1fbec8bbdb2090
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-07-08 10:55:24 -0300

    feat(filter): load providers at runtime and make FilterMenu provider-aware

--- src/app/filter/template.tsx
+++ src/app/filter/template.tsx
@@ -2,14 +2,16 @@ import { Suspense } from "react";
 
 import FilterMenu from "@/app/filter/components/FilterMenu";
 import { QueryProvider } from "@/components/Providers";
+import { getMovieProviders } from "@/lib/api/tmdb/use-cases/getMovieProviders";
 
 type TemplateProps = { children: React.ReactNode };
 export const revalidate = 86400;
 
-export default function Template({ children }: Readonly<TemplateProps>) {
+export default async function Template({ children }: Readonly<TemplateProps>) {
+  const [listProviders] = await getMovieProviders({});
   return (
     <Suspense>
-      <FilterMenu>
+      <FilterMenu listProviders={listProviders}>
         <QueryProvider>{children}</QueryProvider>
       </FilterMenu>
     </Suspense>
